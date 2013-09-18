0b8d0b1dcd86155ad0193208c9d3e1942c3033ed
alexyoung91 <alexyoung91@googlemail.com>
2013-09-18 15:55:21 +0100

    added all set and get functions for sphere properties so far

--- Sphere.java
+++ Sphere.java
@@ -1,20 +1,45 @@
 public class Sphere {
 	private double radius;
 	private double volume;
+	private double surfaceArea;
 
 	public Sphere() {
-		radius = volume = 0.0;
+		radius = volume = surfaceArea = 0.0;
 	}
 	
 	public Sphere(double a_radius) {
 		radius = a_radius;
+		volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+		surfaceArea = 4 * Math.PI * Math.pow(radius, 2);
 	}
 	
 	public void setRadius(double a_radius) {
 		radius = a_radius;
+		volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+		surfaceArea = 4 * Math.PI * Math.pow(radius, 2);
+	}
+	
+	public void setVolume(double a_volume) {
+		volume = a_volume;
+		radius = Math.cbrt(volume / ((4.0 / 3.0) * Math.PI));
+		surfaceArea = 4 * Math.PI * Math.pow(radius, 2);
+	}
+	
+	public void setSurfaceArea(double a_surfaceArea) {
+		surfaceArea = a_surfaceArea;
+		radius = Math.sqrt(surfaceArea / (4 * Math.PI));
+		volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
 	}
 	
 	public double getRadius() {
 		return radius;
+	}
+	
+	public double getVolume() {
+		return volume;
+	}
+	
+	public double getSurfaceArea() {
+		return surfaceArea;
 	}
 }
