61849cb88390c53291a65729fbdf370ca6c3ac76
alexyoung91 <alexyoung91@googlemail.com>
2013-09-18 21:59:09 +0100

    Changed color from string to int (hex)

--- PhysicalSphere.java
+++ PhysicalSphere.java
@@ -6,7 +6,7 @@ public class PhysicalSphere extends Sphere {
 	private double density;
 	private double mass;
 	private double radius;
-	private String color;
+	private int color;
 	
 	public PhysicalSphere(double a_radius, double a_density) {
 		super(a_radius);
@@ -30,7 +30,7 @@ public class PhysicalSphere extends Sphere {
 		return mass;
 	}
 	
-	public void setColor(String a_color) {
+	public void setColor(int a_color) {
 		color = a_color;
 	}
 	
