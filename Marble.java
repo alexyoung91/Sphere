3ab5fc87652341f90a8e47d24f4e8978af353b66
alexyoung91 <alexyoung91@googlemail.com>
2013-09-19 16:34:34 +0100

    modifed Marble constructor

--- Marble.java
+++ Marble.java
@@ -3,9 +3,10 @@ import java.awt.Color;
 public class Marble extends PhysicalSphere {
 	public String manufacturer = "";
 
-	public Marble(double a_radius, double a_density, Color a_color) {
+	public Marble(double a_radius, double a_density, Color a_color, String a_manufacturer) {
 		super(a_radius, a_density);
 		setColor(a_color);
+		manufacturer = a_manufacturer;
 	}
 	
 	public void setManufacturer(String a_manufacturer) {
