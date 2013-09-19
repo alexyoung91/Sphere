6a886f1b5959648c45f9714667bf95bddd655608
alexyoung91 <alexyoung91@googlemail.com>
2013-09-19 16:41:19 +0100

    Changed default manufacturer string

--- Marble.java
+++ Marble.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 
 public class Marble extends PhysicalSphere {
-	public String manufacturer = "";
+	public String manufacturer = "default";
 
 	public Marble(double a_radius, double a_density, Color a_color, String a_manufacturer) {
 		super(a_radius, a_density);
