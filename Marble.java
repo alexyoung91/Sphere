9d24f5d69e84d3f3dc903dc441436362ecfbb69c
Joe Standing <joe.at.g1@gmail.com>
2013-09-20 02:22:08 +0100

    Added crappy variable

--- Marble.java
+++ Marble.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 
 public class Marble extends PhysicalSphere {
 	public String manufacturer = "default";
+	public String hello = "He;llo";
 
 	public Marble(double a_radius, double a_density, Color a_color, String a_manufacturer) {
 		super(a_radius, a_density);
