5b3713b4db4961df53d4fe803c57699958ab6b82
alexyoung91 <alexyoung91@googlemail.com>
2013-09-20 02:26:15 +0100

    Removed bullshit, added position

--- Marble.java
+++ Marble.java
@@ -2,7 +2,9 @@ import java.awt.Color;
 
 public class Marble extends PhysicalSphere {
 	public String manufacturer = "default";
-	public String hello = "He;llo";
+	public int x = 0;
+	public int y = 0;
+	public int z = 0;
 
 	public Marble(double a_radius, double a_density, Color a_color, String a_manufacturer) {
 		super(a_radius, a_density);
